7d1f1a4a7680cfa825302004f2c19ad4f627af91
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:33:50 +0530

    Update robots_sitemap.py

--- checks/robots_sitemap.py
+++ checks/robots_sitemap.py
@@ -1,9 +1,13 @@
 import requests
+
 def run(url, html_content=None):
     domain = url.split("//")[-1].split("/")[0]
     robots_url = f"https://{domain}/robots.txt"
     sitemap_url = f"https://{domain}/sitemap.xml"
-    robots_status = "found" if requests.get(robots_url, timeout=5).status_code == 200 else "not found"
-    sitemap_status = "found" if requests.get(sitemap_url, timeout=5).status_code == 200 else "not found"
-    return {"robots.txt": robots_status, "sitemap.xml": sitemap_status}
-  
+    try:
+        r_status = "found" if requests.get(robots_url,timeout=5).status_code==200 else "not found"
+    except: r_status = "not found"
+    try:
+        s_status = "found" if requests.get(sitemap_url,timeout=5).status_code==200 else "not found"
+    except: s_status="not found"
+    return {"robots.txt": r_status, "sitemap.xml": s_status}
