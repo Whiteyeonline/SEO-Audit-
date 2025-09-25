00263bcb4bdae48bc4eaea61b9045b04f496240a
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:34:19 +0530

    Update canonical_check.py

--- checks/canonical_check.py
+++ checks/canonical_check.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
-    canonical = soup.find("link", attrs={"rel": "canonical"})
-    canonical_url = canonical.get("href") if canonical else None
-    return {"canonical_url": canonical_url, "match": (canonical_url == url) if canonical_url else False}
-  
+    canonical = soup.find("link", rel="canonical")
+    c_url = canonical.get("href") if canonical else None
+    return {"canonical_url": c_url, "match": (c_url==url) if c_url else False}
