6ca29cf45ae5157b217ad27f0f2c9d193491e924
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:34:43 +0530

    Update schema_check.py

--- checks/schema_check.py
+++ checks/schema_check.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
-    schemas = [script.get("type") for script in soup.find_all("script") if script.get("type") and "json" in script.get("type")]
+    schemas = [s.get("type") for s in soup.find_all("script") if s.get("type") and "json" in s.get("type")]
     return {"schema_tags": schemas, "found_count": len(schemas)}
-  
