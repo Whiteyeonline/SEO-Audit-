9ccf45401943cc8a8f04da539d73caa4c0d398ae
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:30:46 +0530

    Update content_quality.py

--- checks/content_quality.py
+++ checks/content_quality.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
+import textstat
+
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
     text = soup.get_text(separator=' ', strip=True)
-    return {"word_count": len(text.split()), "note": "A higher word count can indicate more detailed content."}
-  
+    word_count = len(text.split())
+    readability = textstat.flesch_reading_ease(text)
+    return {"word_count": word_count, "readability_score": readability}
