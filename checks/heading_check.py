915a0d3b9e747b98945d60cb31ac5686ea605fb9
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:28:43 +0530

    Update heading_check.py

--- checks/heading_check.py
+++ checks/heading_check.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
-    return {h: len(soup.find_all(h)) for h in ["h1", "h2", "h3", "h4", "h5", "h6"]}
-  
+    counts = {h: len(soup.find_all(h)) for h in ["h1","h2","h3","h4","h5","h6"]}
+    return counts
