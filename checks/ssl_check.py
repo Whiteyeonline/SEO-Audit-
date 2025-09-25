de9c31f53393006d9c50c3d6680d8cd8257bd39a
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:33:18 +0530

    Update ssl_check.py

--- checks/ssl_check.py
+++ checks/ssl_check.py
@@ -1,13 +1,13 @@
 import ssl, socket
+
 def run(url, html_content=None):
     try:
         domain = url.split("//")[-1].split("/")[0]
         ctx = ssl.create_default_context()
         with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
             s.settimeout(5)
-            s.connect((domain, 443))
+            s.connect((domain,443))
             cert = s.getpeercert()
-            return {"valid_ssl": True, "issuer": cert.get("issuer", [])}
+            return {"valid_ssl": True, "issuer": cert.get("issuer",[])}
     except:
         return {"valid_ssl": False, "issuer": None}
-      
