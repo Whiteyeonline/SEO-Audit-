a578d19277c5375cd60dc31f1a2ea52d7bc937c2
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:29:46 +0530

    Update link_check.py

--- checks/link_check.py
+++ checks/link_check.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
     links = [a.get("href") for a in soup.find_all("a", href=True)]
     broken = []
-    return {"total": len(links), "broken": broken, "note": "Broken link check is disabled for speed."}
-  
+    for link in links:
+        if not link.startswith("http"):
+            continue
+        try:
+            r = requests.head(link, allow_redirects=True, timeout=5)
+            if r.status_code >= 400:
+                broken.append(link)
+        except:
+            broken.append(link)
+    return {"total": len(links), "broken": broken}
