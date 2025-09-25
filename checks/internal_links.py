e8bedace0c132afb9d3deb8b47d08dd3e8f4424a
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:30:18 +0530

    Update internal_links.py

--- checks/internal_links.py
+++ checks/internal_links.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
-    internal_links = [a['href'] for a in soup.find_all('a', href=True) if a['href'].startswith('/') or url.split("//")[-1] in a['href']]
-    return {"internal_link_count": len(internal_links), "note": "Count of internal links on the page."}
-  
+    domain = url.split("//")[-1].split("/")[0]
+    internal = [a['href'] for a in soup.find_all('a', href=True)
+                if a['href'].startswith('/') or domain in a['href']]
+    return {"internal_link_count": len(internal), "links": internal}
