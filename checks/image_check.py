6bc87c7f7351b595bb5d378df4b33980dd22910a
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-25 18:29:12 +0530

    Update image_check.py

--- checks/image_check.py
+++ checks/image_check.py
@@ -2,5 +2,5 @@ from bs4 import BeautifulSoup
 def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
     imgs = soup.find_all("img")
-    return {"total": len(imgs), "missing_alt": sum(1 for img in imgs if not img.get("alt"))}
-  
+    missing_alt = [img.get("src") for img in imgs if not img.get("alt")]
+    return {"total": len(imgs), "missing_alt": len(missing_alt), "missing_list": missing_alt}
