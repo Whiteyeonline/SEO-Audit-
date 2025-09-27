a6494c96f60ce59c2692e6b523a0818b6078793e
Whiteyeonline <www.muneerjahan@gmail.com>
2025-09-27 09:12:16 +0530

    Update image_check.py

--- checks/image_check.py
+++ checks/image_check.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
-def run(url, html_content):
+def run(url, html_content): # url parameter added to match main.py signature
     soup = BeautifulSoup(html_content, "lxml")
     imgs = soup.find_all("img")
-    missing_alt = [img.get("src") for img in imgs if not img.get("alt")]
+    # Only count images that have a source attribute (i.e., real images)
+    missing_alt = [img.get("src") for img in imgs if not img.get("alt") and img.get("src")]
     return {"total": len(imgs), "missing_alt": len(missing_alt), "missing_list": missing_alt}
+    
