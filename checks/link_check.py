81b9dec185823f46a449651bf88b895dc3a21734
Whiteyeonline <www.muneerjahan@gmail.com>
2025-10-02 21:09:35 +0530

    Update link_check.py

--- checks/link_check.py
+++ checks/link_check.py
@@ -5,13 +5,23 @@ def run(url, html_content):
     soup = BeautifulSoup(html_content, "lxml")
     links = [a.get("href") for a in soup.find_all("a", href=True)]
     broken = []
-    for link in links:
-        if not link.startswith("http"):
-            continue
-        try:
-            r = requests.head(link, allow_redirects=True, timeout=5)
-            if r.status_code >= 400:
-                broken.append(link)
-        except:
-            broken.append(link)
-    return {"total": len(links), "broken": broken}
+    
+    # NOTE: The following block for live link checking has been commented out 
+    # for performance and stability reasons (rate-limiting/bans).
+    #
+    # for link in links:
+    #     if not link.startswith("http"):
+    #         continue
+    #     try:
+    #         r = requests.head(link, allow_redirects=True, timeout=5)
+    #         if r.status_code >= 400:
+    #             broken.append(link)
+    #     except:
+    #         broken.append(link)
+            
+    return {
+        "total": len(links), 
+        "broken": broken,
+        "note": "Live broken link check is DISABLED for performance. Only link counts are reported."
+    }
+    
