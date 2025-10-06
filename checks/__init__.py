7b5bbac16a2bf7f2c6c33e024aa7fb4b7c3adeb5
Whiteyeonline <www.muneerjahan@gmail.com>
2025-10-06 20:04:34 +0530

    Update __init__.py

--- checks/__init__.py
+++ checks/__init__.py
@@ -1,29 +1,8 @@
-# checks/__init__.py
-# This file must explicitly list all module names so they can be imported 
-# directly from the 'checks' package (e.g., from checks import ssl_check).
-
-# Core Infrastructure/Technical Checks
-from . import ssl_check
-from . import robots_sitemap
-from . import performance_check
-from . import url_structure
-from . import canonical_check
-from . import link_check
-from . import internal_links
-
-# On-Page Element Checks
-from . import meta_check
-from . import heading_check
-from . import image_check
-from . import schema_check
-
-# Content Quality & Keyword Checks
-from . import keyword_analysis
-from . import content_quality
-
-# Specialized/Miscellaneous Checks
-from . import accessibility_check
-from . import mobile_friendly_check
-from . import analytics_check
-from . import backlinks_check
-from . import local_seo_check 
+# checks/__init__.py (Code to be included)
+from . import (
+    ssl_check, robots_sitemap, performance_check, keyword_analysis, 
+    local_seo_check, meta_check, heading_check, image_check, link_check,
+    schema_check, url_structure, internal_links, canonical_check,
+    content_quality, accessibility_check, mobile_friendly_check,
+    backlinks_check, analytics_check
+)
